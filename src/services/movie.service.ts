35c102e0ac99be62d3ad47946784c054a41f70d1
Athul krishna K S <athulkrishna736@gmail.com>
2026-01-21 00:00:00 +0530

    refactor update services movies

--- src/services/movie.service.ts
+++ src/services/movie.service.ts
@@ -12,23 +12,23 @@ export class MovieService implements IMovieService {
         @inject(TOKENS.FavoritesRepository) private _favoriteRepository: IRepository<IMovie>,
     ) { }
 
-    async searchMovies(query: string, page: number, limit: number): Promise<{ data: IMovie[]; total: number; message: string }> {
-        const omdbPage = Math.ceil((page * limit) / 10);
+    // async searchMovies(query: string, page: number, limit: number): Promise<{ data: IMovie[]; total: number; message: string }> {
+    //     const omdbPage = Math.ceil((page * limit) / 10);
 
-        const response = await axios.get(`${CONFIGS.OMDB_URL}/?apikey=${CONFIGS.OMDB_API_KEY}&s=${query}&page=${omdbPage}`);
+    //     const response = await axios.get(`${CONFIGS.OMDB_URL}/?apikey=${CONFIGS.OMDB_API_KEY}&s=${query}&page=${omdbPage}`);
 
-        const results = response.data?.Search ?? [];
-        const totalResults = Number(response.data?.totalResults ?? results.length);
+    //     const results = response.data?.Search ?? [];
+    //     const totalResults = Number(response.data?.totalResults ?? results.length);
 
-        const startIndex = ((page - 1) * limit) % 10;
-        const paginated = results.slice(startIndex, startIndex + limit);
+    //     const startIndex = ((page - 1) * limit) % 10;
+    //     const paginated = results.slice(startIndex, startIndex + limit);
 
-        return {
-            data: paginated,
-            total: totalResults,
-            message: "Movies fetched successfully",
-        };
-    }
+    //     return {
+    //         data: paginated,
+    //         total: totalResults,
+    //         message: "Movies fetched successfully",
+    //     };
+    // }
 
     getFavorites(): { movies: IMovie[], message: string } {
         const favorites = this._favoriteRepository.read();
